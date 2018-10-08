9fe4c49662c062a738aaa1f3292ab8acbf0cb451
Asama <asamahayder@yahoo.dk>
2018-10-08 15:31:17 +0200

    eksperimentel commit: "linje 8"

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
         System.out.println("Hej med mig også ");
         System.out.println("den her kommentar undersøger merge konlfikter");
         System.out.println("change");
-
+        System.out.println("linje 8");
     }
 }
