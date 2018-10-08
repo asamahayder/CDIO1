0ff1e1e6662a9f678f972a8d5df7b6333be145d9
Asama <asamahayder@yahoo.dk>
2018-10-08 15:46:54 +0200

    eksperimentel commit: reset, fam

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-       int i =0;
-       while (i <10){
-           System.out.println(i);
-           i++;
-       }
-        int t = 5 ;
-        if( t < 5 ){
-            System.out.println(" Hej ");
-
-        }
-        else{
-            System.out.println(" Hej hej");
-        }
+        System.out.println("Hello World!");
     }
 }
