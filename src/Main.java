ba2889a1376edd84ca4b680520d262771d425c08
Jákup VIljam Dam <jayveedees@gmail.com>
2018-10-08 15:23:18 +0200

    experimentalt et eller andet

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hej med dig");
+        System.out.println("hej med mig");
+
+        System.out.println("What is the meaning of life, nani hlolol asdasdsadsadasdasdjjdjfhfhf");
     }
 }
